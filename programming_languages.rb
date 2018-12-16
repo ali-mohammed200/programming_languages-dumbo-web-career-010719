157d8dc97dec99ece35eb1bf3d1279ffbde2b8f7
Mohammad Ali <ali.mohammed200@gmail.com>
2018-12-16 00:52:09 +0000

    Automatically backed up by Learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -4,7 +4,7 @@ def reformat_languages(languages)
   languages.each {|styleName, langsObj|
     langsObj.each {|lang, itsDescripObj|
       new_hash[lang] = itsDescripObj
-      keysofHash = new_hash[lang].keys
+      keysofHash = new_hash.keys
       if keysofHash.include?(:style)
         new_hash[lang][:style].push(styleName)
       else
